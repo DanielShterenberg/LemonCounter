ad99b1b8e7495e3171326561a74d5776bc8e5bdf
Daniel Shterenberg <15daniel10@gmail.com>
2020-06-27 20:30:03 +0300

    removing stuff

--- counters.py
+++ counters.py
@@ -16,10 +16,7 @@ def count_words_from_string(word_counter, raw_data):
     update_counters(word_counter, raw_data)
 
 
-# todo: consider the scenario where the "url-file" is really big as well
-# todo: consider using requests https://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python
 # example: "https://www.w3.org/TR/PNG/iso_8859-1.txt"
-# https://kite.com/python/answers/how-to-read-a-text-file-from-a-url-in-python
 def count_words_from_url(word_counter, url):
     http = urllib3.PoolManager()
     r = http.request('GET', url, preload_content=False)
@@ -28,7 +25,7 @@ def count_words_from_url(word_counter, url):
         update_counters(word_counter, line)
 
 
-# Example: "../therealslimshady.txt"
+# Example: "../mynameis.txt"
 def count_words_from_path(word_counter, path):
     logging.info("data is a path type")
     start_time = time.time()
@@ -49,7 +46,6 @@ def parellalize_count(word_counter, path):
     cores = get_number_of_cores_to_use()
     logging.info(f"using {cores} cores to process the file")
 
-    # init objects
     pool = mp.Pool(cores)
     jobs = []
 
@@ -57,11 +53,10 @@ def parellalize_count(word_counter, path):
     for chunk_start, chunk_size in generate_chunks(path):
         jobs.append(pool.apply_async(process_wrapper, (word_counter, path, chunk_start, chunk_size)))
 
-    # wait for all jobs to finish
+    # wait for all the jobs to finish
     for job in jobs:
         job.get()
 
-    # clean up
     pool.close()
 
 
@@ -92,7 +87,6 @@ def process_wrapper(word_counter, path, chunk_start, chunk_size):
 def update_counters(word_counter, line):
     line = filter_line(line)
 
-    # print(line)
     for word in re.findall(r'\w+', line):
         if word in word_counter:
             word_counter[word] += 1
@@ -101,13 +95,7 @@ def update_counters(word_counter, line):
 
 
 def filter_line(line):
-    #  ignored_chars = '''~`./!@#$%^&*()?1234567890-_+=<>,:;"'{}[]'''
-    # line.replace("-", " ").replace(",", " ").replace(".", " ")
-    # line = re.sub('[-,.]', ' ', line)
     line = re.sub(r'[^A-Za-z\s]+', ' ', line)
-    # re.sub('[^A-Za-z ]+', '', line)
-
-    # return "".join(filter(lambda x: x not in ignored_chars, line.lower()))
     return line.lower()
 
 
